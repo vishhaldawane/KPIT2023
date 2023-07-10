7044dab4789fd49f9d83be268cebb65681f68965
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-07-10 15:20:28 +0530

    Runnable

--- ThreadTest.java
+++ ThreadTest.java
@@ -6,18 +6,30 @@ public class ThreadTest {
 		Train myTrain = new Train("\tChennai Express");
 		Car myCar3 = new Car("\t\tKIA");
 		
-		//4. never call run, rather 
-		//call its start() method, which invokes the run()
-		myCar3.start();//will invoke run()
-		myTrain.start();//will invoke run()
-		myCar1.start();//will invoke run();
+		//4. Create the object of java.lang.Thread class 
+		//and pass the Runnable object in it's constructor
+		
+		Thread t1 = new Thread(myCar1);
+		Thread t2 = new Thread(myTrain);
+		Thread t3 = new Thread(myCar3);
+		
+		//5. never call run, rather 
+		//call t1's start() method, which invokes Runnable's run()
+		
+		t1.start();//will invoke myCar1's run()
+		t2.start();//will invoke myTrain's run()
+		t3.start();//will invoke myCar3's run();
 		
 				
 	}
 }
 
-//1. extend from java.lang.Thread class
-class Car extends Thread {
+class Vehicle {
+	
+}
+
+//1. implements from java.lang.Runnable interface
+class Car extends Vehicle implements Runnable {
 	
 	String model;
 	
@@ -25,7 +37,7 @@ class Car extends Thread {
 		System.out.println("Car() constructed...");
 		this.model = model;
 	}
-	//2. override its run method
+	//2. mandate to implement run method
 	public void run() {
 		//put your logic here, that participates in threading
 		for (int i = 0; i < 100; i++) {
@@ -34,7 +46,7 @@ class Car extends Thread {
 	}
 }
 
-class Train extends Thread {
+class Train extends Vehicle implements Runnable {
 	
 	String model;
 	
@@ -42,7 +54,7 @@ class Train extends Thread {
 		System.out.println("Train() constructed...");
 		this.model = model;
 	}
-	//2. override its run method
+	//2. mandate
 	public void run() {
 		//put your logic here, that participates in threading
 		for (int i = 0; i < 200; i++) {
