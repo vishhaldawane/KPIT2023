5a0e3b3934d72c8e1dfbf9e397a076fb9cf00c15
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2023-07-11 18:29:06 +0530

    Maven Test

---
+++ TestCase1.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class TestCase1 {
+
+	
+	
+	
+	@Test
+	public void test1() {
+		System.out.println("Test case 1 ....");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("Test case 2 ....");
+	}
+	
+
+	@Test
+	public void test3() {
+		System.out.println("Test case 3....");
+	}
+
+}
